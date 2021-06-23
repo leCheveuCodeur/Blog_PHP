372be79358934a50af3d709f075a173ad02b7277
leCheveuCodeur <julien.jamet37pro@gmail.com>
2021-06-23 13:48:24 +0200

    front/add user.login & user.add view

--- app/Views/user/add.php
+++ app/Views/user/add.php
@@ -1,6 +1,8 @@
-<form method='post'>
-    <?= $form->input('username', 'Pseudo'); ?>
-    <?= $form->input('email', 'Adresse mail', ['type' => 'email']); ?>
-    <?= $form->input('password', 'Mot de passe', ['type' => 'password']); ?> <?= $form->input('password2', 'Confirmation du mot de passe', ['type' => 'password']); ?>
-    <button class='btn btn-primary' type='submit'>Envoyer</button>
-</form>
+<div class="container-xxl text-dark">
+    <form class="user-log" method='post'>
+        <?= $form->input('username', 'Pseudo'); ?>
+        <?= $form->input('email', 'Adresse mail', ['type' => 'email']); ?>
+        <?= $form->input('password', 'Mot de passe', ['type' => 'password']); ?> <?= $form->input('password2', 'Confirmation du mot de passe', ['type' => 'password']); ?>
+        <button class='btn btn-primary' type='submit'>Créer le compte</button>
+    </form>
+</div>
--- app/Views/user/login.php
+++ app/Views/user/login.php
@@ -1,6 +1,8 @@
-<form method="post">
-    <?= $form->input("usernameOrEmail", "Pseudo ou Email"); ?>
-    <?= $form->input("password", "Mot de passe", ["type" => "password"]); ?>
-    <button class="btn btn-primary" type="submit">Envoyer</button>
-</form>
-<a href="?p=user.add&return=user.login">Créer un compte</a>
+<div class="container-xxl text-dark">
+    <form class="user-log text mx-auto" method="post">
+        <?= $form->input("usernameOrEmail", "Pseudo ou Email"); ?>
+        <?= $form->input("password", "Mot de passe", ["type" => "password"]); ?>
+        <button class="btn btn-primary" type="submit">Connexion</button>
+    </form>
+    <a class="d-block text-center mt-5" href="?p=user.add&return=user.login">Créer un compte</a>
+</div>
